ead6790fbddabfc61dbe28241efa6fcfc2f99cf4
crisstanza <crisstanza@home>
2018-07-11 23:14:29 -0300

    Correção nos cálculos de anos e meses.

--- js/lib/Utils.DATE.js
+++ js/lib/Utils.DATE.js
@@ -15,16 +15,32 @@ if (!Utils) { var Utils = {}; }
 	};
 
 	Utils.DATE.years = function(date1, date2) {
-		return date1.getFullYear() - date2.getFullYear();
+		var month1 = Utils.DATE.month(date1);
+		var month2 = Utils.DATE.month(date2);
+		var diff;
+		if (month1 < month2) {
+			diff = 1;
+		} else {
+			diff = 0;
+		}
+		return date1.getFullYear() - date2.getFullYear() - diff;
 	};
 
 	Utils.DATE.months = function(date1, date2) {
 		var month1 = Utils.DATE.month(date1);
 		var month2 = Utils.DATE.month(date2);
-		if (month1 >= month2) {
-			return month1 - month2;
+		var day1 = date1.getDate();
+		var day2 = date2.getDate();
+		var diff;
+		if (day1 < day2) {
+			diff = 1;
 		} else {
-			return 12 - month2 + month1;
+			diff = 0;
+		}
+		if (month1 >= month2) {
+			return month1 - month2 - diff;
+		} else {
+			return 12 - month2 + month1 - diff;
 		}
 	};
 
